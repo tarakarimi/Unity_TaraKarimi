0a4da37115dfe841e855f0ee44635b1f846fc614
Tara Karimi <tarakarimi1377@gmail.com>
2023-07-14 18:44:43 +0330

    Player movement bounds

--- Space Shooter Pro/Assets/Scripts/Player.cs
+++ Space Shooter Pro/Assets/Scripts/Player.cs
@@ -24,5 +24,22 @@ public class Player : MonoBehaviour
         //transform.Translate(Vector3.up * _speed * verticalInput * Time.deltaTime);
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
+        
+        //player bounds
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        } else if (transform.position.y <= -4.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.8f, 0);
+        }
+
+        if (transform.position.x >= 17)
+        {
+            transform.position = new Vector3(-17, transform.position.y, 0);
+        } else if (transform.position.x < -17)
+        {
+            transform.position = new Vector3(17, transform.position.y, 0);
+        }
     }
 }

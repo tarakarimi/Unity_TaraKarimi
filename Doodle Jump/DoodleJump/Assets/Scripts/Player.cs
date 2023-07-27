efaa0e56a33e26931d8d27acb4323a7e6931d77c
Tara Karimi <68959989+tarakarimi@users.noreply.github.com>
2023-07-27 15:19:15 +0330

    Flip the Doodler to left & right

--- Doodle Jump/DoodleJump/Assets/Scripts/Player.cs
+++ Doodle Jump/DoodleJump/Assets/Scripts/Player.cs
@@ -8,22 +8,38 @@ public class Player : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
 
     private float horizontalMovement = 0f;
-
     private float movementSpeed = 5f;
+    [SerializeField] private Sprite leftSprite;
+    [SerializeField] private Sprite rightSprite;
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = rightSprite;
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalMovement = Input.GetAxis("Horizontal") * movementSpeed;
+        FlipCharacterHandler();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontalMovement, rb.velocity.y) ;
+    }
+
+    void FlipCharacterHandler()
+    {
+        if (horizontalMovement < 0)
+        {
+            spriteRenderer.sprite = leftSprite;
+        }
+        else if (horizontalMovement > 0)
+        {
+            spriteRenderer.sprite = rightSprite;
+        }
     }
 }

f48e55c852289bb2f45cc53f84b1258d5ec50750
TaraKarimi <tara.karimi@icloud.com>
2023-08-06 13:22:23 +0330

    Level generation modified
    - no two jumpable platforms are further than 1.5f

--- Doodle Jump/DoodleJump/Assets/Scripts/LevelGenerator.cs
+++ Doodle Jump/DoodleJump/Assets/Scripts/LevelGenerator.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject platformPrefab;
+    public GameObject breakablePlatformPrefab;
+    public GameObject moveablePlatformPrefab;
     private int numberOfPlatforms = 50;
     [SerializeField] private float levelWidth = 2.6f;
     [SerializeField] private float minY = 0.4f;
     [SerializeField] private float maxY = 1.5f;
     [SerializeField] private GameObject platformParent;
-    Vector3 spawnPosition;
+    private Vector3 spawnPosition;
     [SerializeField] private GameObject firstPlat;
-    [SerializeField] private GameObject breakablePlatformPrefab;
-    [SerializeField] private GameObject movablePlatformPrefab;
-    
+
+    private bool lastPlatformWasBreakable = false;
+    private float lastNonBreakableXPosition = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         spawnPosition = firstPlat.transform.position;
+        lastNonBreakableXPosition = spawnPosition.x;
         SpawnPlatforms();
     }
 
@@ -30,42 +34,49 @@ public class LevelGenerator : MonoBehaviour
             SpawnPlatforms();
         }
     }
-    
+
     public void SpawnPlatforms()
     {
         for (int i = 0; i < numberOfPlatforms; i++)
         {
-            spawnPosition.y += Random.Range(minY, maxY);
-            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            GameObject prefabToSpawn;
 
-            // Check if the current spawn position will obstruct the doodler's jumpable path.
-            bool canSpawn = !Physics2D.Raycast(spawnPosition, Vector2.down, maxY * 2);
-
-            if (canSpawn)
+            if (lastPlatformWasBreakable || Random.Range(0f, 1f) > 0.5f)
             {
-                // Determine platform type to spawn.
-                GameObject platformToSpawn = platformPrefab;
-                float randomValue = Random.value;
-
-                if (randomValue < 0.2f) // 20% chance for breakable platform.
-                    platformToSpawn = breakablePlatformPrefab;
-                else if (randomValue < 0.4f) // 20% chance for movable platform.
-                    platformToSpawn = movablePlatformPrefab;
-
-                // Instantiate the selected platform.
-                GameObject tempPlat = Instantiate(platformToSpawn, spawnPosition, Quaternion.identity);
-                tempPlat.transform.parent = platformParent.transform;
+                prefabToSpawn = platformPrefab;
+                lastPlatformWasBreakable = false;
             }
-        }
-    }
-    /*public void SpawnPlatforms()
-    {
-        for (int i = 0; i < numberOfPlatforms; i++)
-        {
-            spawnPosition.y += Random.Range(minY, maxY);
-            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
-            GameObject tempPlat = Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+            else if (Random.Range(0f, 1f) > 0.5f)
+            {
+                prefabToSpawn = breakablePlatformPrefab;
+                lastPlatformWasBreakable = true;
+            }
+            else
+            {
+                prefabToSpawn = moveablePlatformPrefab;
+                lastPlatformWasBreakable = false;
+            }
+
+            float platformScale = prefabToSpawn.transform.localScale.x;
+            float platformWidth = platformScale;
+
+            float xPosition;
+
+            if (lastPlatformWasBreakable)
+            {
+                float minX = Mathf.Max(-levelWidth, lastNonBreakableXPosition - 1.5f);
+                float maxX = Mathf.Min(levelWidth, lastNonBreakableXPosition + 1.5f);
+                xPosition = Random.Range(minX, maxX);
+                lastNonBreakableXPosition = xPosition;
+            }
+            else
+            {
+                xPosition = Random.Range(-levelWidth, levelWidth);
+            }
+
+            spawnPosition = new Vector3(xPosition, spawnPosition.y + Random.Range(minY, maxY), 0f);
+            GameObject tempPlat = Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
             tempPlat.transform.parent = platformParent.transform;
         }
-    }*/
+    }
 }

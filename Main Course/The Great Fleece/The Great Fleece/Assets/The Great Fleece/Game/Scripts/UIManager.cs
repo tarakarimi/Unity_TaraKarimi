f400b5c20d8ca7595f6e9c1a6f51522d984becb8
TaraKarimi <tara.karimi@icloud.com>
2023-09-26 14:58:36 +0330

    Menu added

---
+++ Main Course/The Great Fleece/The Great Fleece/Assets/The Great Fleece/Game/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+
+    public void Restart()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
